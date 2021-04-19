8f62921c3d37501c6540b8df814a9c061fc3497f
vati <66106915+Vatii123@users.noreply.github.com>
2021-04-19 21:04:44 +0700

    added C folder

---
+++ c/helloWelcoem1.c
@@ -0,0 +1,13 @@
+/* this was originally written in repl.it not on vscode i usually do k */
+
+#include <stdio.h>
+
+int main() {
+
+    int a = 2;
+    while (a = 2) {
+        printf("hello");
+        wait(1);
+        printf("welcoem");
+    }
+}
